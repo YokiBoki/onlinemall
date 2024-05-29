88c69616661a586f3a1ac5c4995f408a40b9ef91
YokiBoki <101558425+YokiBoki@users.noreply.github.com>
2024-05-29 16:32:43 +0800

    complete posts (#18)

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -26,6 +26,7 @@ const routes = [
       { path: 'category', name: 'Category', meta: { name: '分类信息' }, component: () => import('../views/manager/Category') },
       { path: 'goods', name: 'Goods', meta: { name: '商品信息1' }, component: () => import('../views/manager/Goods') },
       { path: 'circles', name: 'Circles', meta: { name: '小圈1' }, component: () => import('../views/manager/Circles') },
+      { path: 'posts', name: 'Posts', meta: { name: '论坛' }, component: () => import('../views/manager/Posts') },
 
     ]
   },
